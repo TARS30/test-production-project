647d62ee8360e31ebd28bff2d15fb5ef22a7e37a
TARS30 <chernovalexandr30@gmail.com>
2023-12-02 07:53:54 +0300

    test

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,9 +1,8 @@
 import { Theme, useTheme } from 'app/providers/ThemeProvider';
 import { classNames } from 'shared/lib/classNames/classNames';
-
+import DarkIcon from 'shared/assets/icons/DarkIcon.svg';
+import LightIcon from 'shared/assets/icons/LightIcon.svg';
 import { Button, ThemeButton } from 'shared/ui/Button/Button';
-import DarkIcon from '../../../shared/assets/icons/theme_Dark.svg';
-import LightIcon from '../../../shared/assets/icons/theme_Light.svg';
 
 interface ThemeSwitcherProps {
   className?: string
