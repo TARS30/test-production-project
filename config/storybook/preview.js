0f1bd641f48a70b56f2c329aefbe8d9df87107b1
TARS30 <chernovalexandr30@gmail.com>
2024-02-15 17:09:01 +0300

    preview

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -23,11 +23,11 @@
 // addDecorator(RouterDecorator);
 // addDecorator(SuspenseDecorator);
 
-import { Theme } from '../src/app/providers/ThemeProvider/lib/ThemeContext';
-import { RouterDecorator } from '../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from '../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { SuspenseDecorator } from '../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { ThemeDecorator } from '../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 const preview = {
   parameters: {
