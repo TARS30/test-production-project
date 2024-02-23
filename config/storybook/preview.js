59575bfb5fb85ff03a226ffde44fe0b7c7e7b489
Tars30 <chernovalexandr30@gmail.com>
2024-02-23 17:30:03 +0300

    stories

--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -23,11 +23,37 @@
 // addDecorator(RouterDecorator);
 // addDecorator(SuspenseDecorator);
 
-import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
-import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+// import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+// import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+// import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+// import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+
+// const preview = {
+//   parameters: {
+//     actions: { argTypesRegex: '^on[A-Z].*' },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/i,
+//       },
+//     },
+//   },
+//   decorators: [
+//     ThemeDecorator(Theme.LIGHT),
+//     RouterDecorator,
+//     SuspenseDecorator,
+//     StyleDecorator,
+//   ],
+// };
+
+// export default preview;
+
+import { Theme } from '../src/app/providers/ThemeProvider/lib/ThemeContext';
+import { RouterDecorator } from '../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { SuspenseDecorator } from '../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { ThemeDecorator } from '../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 const preview = {
   parameters: {
