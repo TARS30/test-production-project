24dd913786e79e052d60bab20a61a94e59adcd64
Tars30 <chernovalexandr30@gmail.com>
2024-02-23 17:35:11 +0300

    stories

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,3 +1,27 @@
+// import { addDecorator } from '@storybook/react';
+// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+// import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+// import { Theme } from '../../src/app/providers/ThemeProvider';
+// import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+// import {
+//   SuspenseDecorator,
+// } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+
+// export const parameters = {
+//   actions: { argTypesRegex: '^on[A-Z].*' },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+
+// };
+
+// addDecorator(StyleDecorator);
+// addDecorator(ThemeDecorator(Theme.LIGHT));
+// addDecorator(RouterDecorator);
+// addDecorator(SuspenseDecorator);
 import { BuildPaths } from './../config/build/types/config';
 import type { StorybookConfig } from '@storybook/react-webpack5';
 import path from 'path';
