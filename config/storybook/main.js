913582e92a0eecf340aa4a0b429bdcc55bddc13f
Tars30 <chernovalexandr30@gmail.com>
2024-02-23 17:34:26 +0300

    stories

--- config/storybook/main.js
+++ config/storybook/main.js
@@ -1,81 +1,14 @@
-// module.exports = {
-//   stories: [
-//     '../../src/**/*.stories.@(js|jsx|ts|tsx)',
-//   ],
-//   addons: [
-//     '@storybook/addon-links',
-//     '@storybook/addon-essentials',
-//     '@storybook/addon-interactions',
-//   ],
-//   framework: '@storybook/react',
-//   core: {
-//     builder: 'webpack5',
-//   },
-// };
-
-import path from 'path';
-import { DefinePlugin } from 'webpack';
-import { buildCssLoader } from '../build/loaders/buildCssLoader';
-
-const config = {
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {},
-  },
-
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
-
+module.exports = {
+  stories: [
+    '../../src/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
     '@storybook/addon-interactions',
-    '@storybook/addon-onboarding',
   ],
-
-  webpackFinal: async (config) => {
-    const paths = {
-      build: '',
-      html: '',
-      entry: '',
-      src: path.resolve(__dirname, '../src'),
-      buildLocales: path.resolve(__dirname, '..', 'build', 'locales'),
-      locales: path.resolve(__dirname, '..', 'public', 'locales'),
-    };
-
-    config?.resolve?.modules?.push(paths.src);
-
-    config?.resolve?.extensions?.push('.ts', '.tsx');
-
-    /// //////////////////////////////////////////////////////////////////////
-    //   svg helper
-    const imageRule = config.module?.rules?.find((rule) => {
-      const { test } = (rule);
-
-      if (!test) {
-        return false;
-      }
-
-      return test.test('.svg');
-    });
-
-    imageRule.exclude = /\.svg$/;
-
-    config?.module?.rules?.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
-    });
-    /// //////////////////////////////////////////////////////////////////////
-
-    config?.module?.rules?.push(buildCssLoader(true));
-
-    config?.plugins?.push(new DefinePlugin({
-      __IS_DEV__: JSON.stringify(true),
-      __API__: JSON.stringify(''),
-      __PROJECT__: JSON.stringify('storybook'),
-    }));
-
-    return config;
+  framework: '@storybook/react',
+  core: {
+    builder: 'webpack5',
   },
 };
-export default config;
--- config/storybook/preview.js
+++ config/storybook/preview.js
@@ -1,59 +1,8 @@
-// import { addDecorator } from '@storybook/react';
-// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-// import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-// import { Theme } from '../../src/app/providers/ThemeProvider';
-// import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-// import {
-//   SuspenseDecorator,
-// } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-
-// export const parameters = {
-//   actions: { argTypesRegex: '^on[A-Z].*' },
-//   controls: {
-//     matchers: {
-//       color: /(background|color)$/i,
-//       date: /Date$/,
-//     },
-//   },
-
-// };
-
-// addDecorator(StyleDecorator);
-// addDecorator(ThemeDecorator(Theme.LIGHT));
-// addDecorator(RouterDecorator);
-// addDecorator(SuspenseDecorator);
-
-// import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
-// import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-// import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-// import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-// import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
-
-// const preview = {
-//   parameters: {
-//     actions: { argTypesRegex: '^on[A-Z].*' },
-//     controls: {
-//       matchers: {
-//         color: /(background|color)$/i,
-//         date: /Date$/i,
-//       },
-//     },
-//   },
-//   decorators: [
-//     ThemeDecorator(Theme.LIGHT),
-//     RouterDecorator,
-//     SuspenseDecorator,
-//     StyleDecorator,
-//   ],
-// };
-
-// export default preview;
-
-import { Theme } from '../src/app/providers/ThemeProvider/lib/ThemeContext';
-import { RouterDecorator } from '../src/shared/config/storybook/RouterDecorator/RouterDecorator';
-import { StyleDecorator } from '../src/shared/config/storybook/StyleDecorator/StyleDecorator';
-import { SuspenseDecorator } from '../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
-import { ThemeDecorator } from '../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider/lib/ThemeContext';
+import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
+import { SuspenseDecorator } from '../../src/shared/config/storybook/SuspenseDecorator/SuspenseDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 
 const preview = {
   parameters: {
