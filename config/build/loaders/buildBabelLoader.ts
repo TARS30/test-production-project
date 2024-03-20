55d77b0ad0b4b50ee5ef67193118956401acef3f
tars30 <chernovalexandr30@gmail.com>
2024-03-20 16:39:11 +0300

    changed webpack config, added babel cache(+auto remove after npm i)

--- config/build/loaders/buildBabelLoader.ts
+++ config/build/loaders/buildBabelLoader.ts
@@ -5,38 +5,42 @@ interface BuildBabelLoaderProps extends BuildOptions {
   isTSX?: boolean;
 }
 
-export const buildBabelLoader = ({ isDev, isTSX }: BuildBabelLoaderProps) => ({
-  test: isTSX ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
-  exclude: /node_modules/,
-  use: {
-    loader: 'babel-loader',
-    options: {
-      presets: ['@babel/preset-env'],
-      plugins: [
-        [
-          'i18next-extract',
-          {
-            locales: ['ru', 'en'],
-            keyAsDefaultValue: true,
-          },
-        ],
-        [
-          '@babel/plugin-transform-typescript',
-          {
-            isTSX,
-          },
-        ],
-        '@babel/plugin-transform-runtime',
-        isTSX && [
-          babelPropsRemovePlugin,
-          {
-            props: [
-              'data-testid',
-            ],
-          },
-        ],
-        isDev && require.resolve('react-refresh/babel'),
-      ].filter(Boolean),
+export const buildBabelLoader = ({ isDev, isTSX }: BuildBabelLoaderProps) => {
+  const isProd = !isDev;
+  return {
+    test: isTSX ? /\.(jsx|tsx)$/ : /\.(js|ts)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        cacheDirectory: true,
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            {
+              locales: ['ru', 'en'],
+              keyAsDefaultValue: true,
+            },
+          ],
+          [
+            '@babel/plugin-transform-typescript',
+            {
+              isTSX,
+            },
+          ],
+          '@babel/plugin-transform-runtime',
+          isTSX && isProd && [
+            babelPropsRemovePlugin,
+            {
+              props: [
+                'data-testid',
+              ],
+            },
+          ],
+          isDev && require.resolve('react-refresh/babel'),
+        ].filter(Boolean),
+      },
     },
-  },
-});
+  };
+};
